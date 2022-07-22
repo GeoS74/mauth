7798bd680a0e19dc150fc9c646f2b56f59564592
GeoS74 <gsirotkin@yandex.ru>
2022-07-22 14:10:56 +0500

    start project

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'no-console': 'off',
+    'linebreak-style': 1,
+    'no-template-curly-in-string': 'off',
+    'no-underscore-dangle': 'off',
+    'no-use-before-define': 'off',
+    'no-restricted-syntax': 'off',
+    'no-await-in-loop': 'off',
+  },
+  ignorePatterns: ['*.test.js', '/files/*'],
+};
