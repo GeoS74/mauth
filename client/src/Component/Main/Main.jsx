78ea9a4cc37b5f434b38919fcebf5871a1dc7169
Aboltys754 <93668286+warwar754@users.noreply.github.com>
2022-07-31 22:16:56 +0500

    на пол дороги 31.07

--- client/src/Component/Main/Main.jsx
+++ client/src/Component/Main/Main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Regform } from "../Regform/Regform";
+import { Authform } from "../Authform/Authform";
 import styles from "./styles.module.css";
 
 export const Main = () => {
--- client/src/Component/Regform/Regform.jsx
+++ client/src/Component/Regform/Regform.jsx
@@ -4,46 +4,94 @@ import classNames from "classnames";
 import { Eye } from "../Eye/Eye";
 import { useState } from "react";
 
-export const Regform = ({ div1, set }) => {
-  const [valueType, setValueType] = useState("password");
+export const Regform = () => {
+  const [valueEyeSlash, setValueEyeSlash] = useState(0);
+
+  const [valueType, setValueType] = useState({
+    value: 1,
+    border: styles.border,
+    passw: styles.pass,
+    name: styles.noneName,
+    buttonName: "Sing in",
+    pName: "Not registered?",
+    aName: "Create an account",
+  });  
 
   const passwordEye = (valueType) => {
-    if (valueType === "password") {
-      valueType = "text";
-    } else if (valueType === "text") {
-      valueType = "password";
+    switch (valueType.value) {
+      case 1:
+        valueEyeSlash.value = 1;
+        valueEyeSlash.passw = styles.pass;
+        valueEyeSlash.name = styles.noneName;
+        valueEyeSlash.buttonName = "Sing in";
+        valueEyeSlash.pName = "Not registered?";
+        valueEyeSlash.aName = "Create an account";
+        break;
+      case 2:
+        valueEyeSlash.value = 2;
+        valueEyeSlash.passw = styles.pass;
+        valueEyeSlash.name = styles.noneName;
+        valueEyeSlash.buttonName = "Sing in";
+        valueEyeSlash.pName = "Not registered?";
+        valueEyeSlash.aName = "Create an account";
+        break;
+      default:
+        valueEyeSlash.value = 0;
+        valueEyeSlash.passw = styles.pass;
+        valueEyeSlash.name = styles.noneName;
+        valueEyeSlash.buttonName = "Sing in";
+        valueEyeSlash.pName = "Not registered?";
+        valueEyeSlash.aName = "Create an account";
     }
-    return valueType;
   };
 
   return (
     <div className={classNames(styles.root)}>
-      <div className={styles.border}>
+      <div className={classNames(valueType.border)}>
         <div className={classNames(styles.email, styles.foo)}>
           <label htmlFor="Email">Email</label>
           <input type="text" id="Email" placeholder="email" />
         </div>
-        <div className={classNames(styles.pass, styles.foo)}>
+        <div className={classNames(valueType.passw, styles.foo)}>
           <div className={styles.paslink}>
             <label htmlFor="Password">Password</label>
+            <a className={classNames(styles.)}
+              onClick={() => setValueType(passwordEye((valueType.value = 1)))}
+              href="#"
+            >
+              Forgot password?
+            </a>
           </div>
           <div className={styles.password}>
             <input type={valueType} placeholder="password" />
-            <button onClick={() => console.log(1)}>
-              <Eye valueSlash={0} />
+            <button
+              onClick={() =>
+                setValueEyeSlash(
+                  valueEyeSlash === 0 ? valueEyeSlash + 1 : valueEyeSlash - 1
+                )
+              }
+            >
+              <Eye valueSlash={valueEyeSlash} />
             </button>
           </div>
         </div>
-        <div className={classNames(styles.name, styles.foo)}>
+        <div className={classNames(valueType.name, styles.foo)}>
           <label htmlFor="Name">Your name - optional</label>
           <input type="text" id="Name" placeholder="name" />
         </div>
-        <button className={classNames(styles.foo)}>Create in account</button>
+        <button className={classNames(styles.foo)}>
+          {valueType.buttonName}
+        </button>
         <div className={classNames(styles.foo, styles.pandlink)}>
           <p>
-            Not registered?
-            <a href="#" onClick={() => set((div1 = 0))}>
-              Sign in
+            {valueType.pName}
+            <a
+              href="#"
+              onClick={() =>
+                setValueEyeSlash(passwordEye((valueType.value = 2)))
+              }
+            >
+              {valueType.aName}
             </a>
           </p>
         </div>
