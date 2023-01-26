c9773795f07ae2b71e61278a7574271bf3a22906
GeoS74 <gsirotkin@yandex.ru>
2023-01-26 18:46:46 +0300

    added @koa/cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const Koa = require('koa');
+const cors = require('@koa/cors')
 
 const errorCatcher = require('./middleware/error.catcher');
 const authRoutes = require('./routes/auth.routes');
@@ -6,6 +7,7 @@ const authRoutes = require('./routes/auth.routes');
 const app = new Koa();
 
 app.use(errorCatcher);
+app.use(cors());
 app.use(authRoutes);
 
 module.exports = app;
