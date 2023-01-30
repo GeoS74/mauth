58cf1fe15fd5665a03dec916bad696b0d9a48674
GeoS74 <gsirotkin@yandex.ru>
2023-01-30 15:44:54 +0500

    CORS control

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const Koa = require('koa');
 const cors = require('@koa/cors')
 
+const config = require('./config');
 const errorCatcher = require('./middleware/error.catcher');
 const authRoutes = require('./routes/auth.routes');
 
 const app = new Koa();
 
 app.use(errorCatcher);
-app.use(cors());
+if (config.node.env === 'dev') {
+  app.use(cors());
+}
 app.use(authRoutes);
 
 module.exports = app;
--- config.js
+++ config.js
@@ -1,6 +1,9 @@
 require('dotenv').config({ path: './secret.env' });
 
 module.exports = {
+  node: {
+    env: process.env.NODE_ENV || 'dev',
+  },
   server: {
     host: process.env.SERVER_HOST || 'localhost',
     port: process.env.SERVER_PORT || 3001,
