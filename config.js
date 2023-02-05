8b557a210e023a9a82a6c9fbc46421a1cb00c376
GeoS74 <gsirotkin@yandex.ru>
2023-02-05 19:38:05 +0300

    fixed .dockerignore

--- config.js
+++ config.js
@@ -24,8 +24,8 @@ module.exports = {
   mailer: {
     host: process.env.MAIL_HOST || 'smtp.ethereal.email',
     port: process.env.MAIL_PORT || 587,
-    user: process.env.MAIL_USER || 'marilyne.dickinson99@ethereal.email',
-    pass: process.env.MAIL_PASS || 'KGNeTT4mU95qtWpDXR',
+    user: process.env.MAIL_USER || 'calista.ledner89@ethereal.email',
+    pass: process.env.MAIL_PASS || 'K3peFMtyuEXehgba8r',
     // secure: false, // true for 465, false for other ports
     // ignoreTLS: true,
   },
